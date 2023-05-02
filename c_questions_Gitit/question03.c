9bc51b03f46de5c3ca1b196ebebaebf289a1cfb0
Avishka Sooriyapperuma <avishkadilshan400@gmail.com>
2023-05-02 22:06:11 +0530

    c question03

---
+++ c_questions_Gitit/question03.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{ float a,b,h;
+printf("Enter the base : ");
+scanf("%f",&b);
+printf("Enter the height : ");
+scanf("%f",&h);
+a=0.5*b*H;
+printf("Area is %f",a);
+}
