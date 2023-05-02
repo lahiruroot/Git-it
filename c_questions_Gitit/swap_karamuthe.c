245e4f11fe9a1dd5c4245e31b9e9ae46ccea3448
Avishka Sooriyapperuma <avishkadilshan400@gmail.com>
2023-05-02 22:28:49 +0530

    swap

---
+++ c_questions_Gitit/swap_karamuthe.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{ int a,b,t;
+printf("Enter the first Number : ");
+scanf("%d",&a);
+printf("Enter the second Number : ");
+scanf("%d",&b);
+a=b;
+b=a;
+a=b;
+//what is that t doing there? kinda sus ryt!
+if (a==b){
+    printf("Wenna bane! Swap wenna one! Aye balanna \n");
+}
+else{
+printf("first number is %d & the second number is %d",a,b);
+}
+}
