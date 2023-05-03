b27f693eb13fc495a65d975d272db5e1a9919c19
Pixelated4Now <senindieminanga@gmail.com>
2023-05-03 14:31:00 -0700

    fix: build issues

--- c_questions_Gitit/factorial.c
+++ c_questions_Gitit/factorial.c
@@ -2,7 +2,7 @@
 int main()
 {
     int n, i;
-    unsigned long long factorial = 0;
+    unsigned long long factorial = 1;
     //unsigned long long is the same as unsigned long long int. 
     //Its size is platform-dependent, but guaranteed by the C standard (ISO C99) to be at least 64 bits. 
 
@@ -10,9 +10,9 @@ int main()
     scanf("%d",&n);
 
     // Show error if number is less than 0
-    if (n < 1)
+    if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
-
+    
     else
     {
         for(i=1; i<=n; ++i)
