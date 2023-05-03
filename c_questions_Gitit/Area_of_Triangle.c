84f759a31b68c5ef1eaa151fe5b2e088eb3a9ec2
Katz2003 <72686198+Katz2003@users.noreply.github.com>
2023-05-03 14:29:38 +0530

    Area_of_Triangle

--- c_questions_Gitit/Area_of_Triangle.c
+++ c_questions_Gitit/Area_of_Triangle.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 void main()
 { float a,b,h;
-printf("Enter the base : ");
-scanf("%f",&b);
-printf("Enter the height : ");
-scanf("%f",&h);
-a=0.5*b*H;
-printf("Area is %f",a);
+    printf("Enter the base : ");
+    scanf("%f",&b);
+    printf("Enter the height : ");
+    scanf("%f",&h);
+    a=0.5*b*h;
+    printf("Area is %f",a);
 }
+
+
+
