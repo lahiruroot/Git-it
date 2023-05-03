c5ec51bafc2188edc4a9510e0f1786036a769b0b
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-05-03 14:23:27 +0530

    Fixed the division

--- c_questions_Gitit/Lets_divide.c
+++ c_questions_Gitit/Lets_divide.c
@@ -1,8 +1,12 @@
-#include<stdio.h>
-void main()
-{ int a,b,c,d;
-printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-c=a/d;
-printf("Division is %d",c);
+#include <stdio.h>
+void main() {
+  float a, b, d;
+  float c;
+  printf("Enter number 1 : ");
+  scanf("%f", &a);
+  printf("Enter the number 2 : ");
+  scanf("%f", &b);
+
+  c = a / b;
+  printf("Division is %f", c);
 }
